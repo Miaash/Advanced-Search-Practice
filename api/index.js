4b2ec223e03960369a89c377c4b1bbcce6aac480
seonga <98681659+hongseonga@users.noreply.github.com>
2022-09-16 01:52:57 +0900

    서치 기능 수정

--- api/index.js
+++ api/index.js
@@ -16,7 +16,9 @@ app.get("/", (req, res) => {
     );
   };
 
+  // 무한스크롤?
   q ? res.json(search(Users).slice(0, 10)) : res.json(Users.slice(0, 10));
 });
 
+// nodemon crush 뜨길래 포트번호를 5000에서 8080으로 바꿔줌
 app.listen(8080, () => console.log("API is working!"));
